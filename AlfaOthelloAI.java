c88b084af4e6224326af7926f52a36f7770694e8
Kasper Dahl Andersen <kdaa@itu.dk>
2021-03-07 12:40:36 +0100

    test

---
+++ AlfaOthelloAI.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class AlfaOthelloAI implements IOthelloAI {
+
+    public Position decideMove(GameState s){
+		ArrayList<Position> moves = s.legalMoves();
+		if ( !moves.isEmpty() )
+			return moves.get(0);
+		else
+			return new Position(-1,-1);
+	}
+    
+}
